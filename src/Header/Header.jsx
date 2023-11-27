c5b5b4f3e337abc4733227d0ea6cb7aba76aca6f
Frederick <120729769+sharpartzgh@users.noreply.github.com>
2023-11-27 09:07:25 +0000

    added the header section

---
+++ src/Header/Header.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header-container'>
+      <img src="\logo\RadofLogo1.png" alt="RadofConsult-Logo" />
+      <ul>
+        <li>Home</li>
+        <li>About Us</li>
+        <li>Services</li>
+        <li>Properties</li>
+        <li>Contact Us</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
